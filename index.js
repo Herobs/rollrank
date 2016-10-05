7de6ec00e24279a2eb28b94b303302d72e4e6319
Herobs <herobs@herobs.cn>
2016-10-06 00:30:55 +0800

    add promise polyfill

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RollRank from './RollRank';
+import promise from 'es6-promise';
+
+promise.polyfill();
 
 ReactDOM.render(<RollRank />, document.querySelector('#rollrank-app'));
